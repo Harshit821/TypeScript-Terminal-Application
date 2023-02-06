1fe4e90136bcab6968389878eaf10bce7dd1b372
Harshit821 <harshit.agarwal212000@gmail.com>
2023-02-06 16:51:02 +0530

    First Commit

---
+++ terminal1.js
@@ -0,0 +1,72 @@
+var ps = require("prompt-sync");
+const prompt=ps();
+var fs = require("fs");
+var path = require("path");
+var command = prompt("Enter the command  ");
+var cmdarray = command.split(" ");
+function pwd() {
+    console.log(process.cwd());
+}
+function ls(dir) {
+    if (dir === void 0) { dir = "."; }
+    fs.readdir(dir, function (err, files) {
+        if (err)
+            throw err;
+        console.log(files.join("\n"));
+    });
+}
+function cp(src, dest) {
+    fs.copyFile(src, dest, function (err) {
+        if (err)
+            throw err;
+        console.log("".concat(src, " was copied to ").concat(dest));
+    });
+}
+;
+function mv(src, dest) {
+    fs.rename(src, dest, function (err) {
+        if (err)
+            throw err;
+        console.log("".concat(src, " was moved to ").concat(dest));
+    });
+}
+;
+function mkdir(dir) {
+    fs.mkdir(dir, function (err) {
+        if (err)
+            throw err;
+        console.log("Directory \"".concat(dir, "\" created"));
+    });
+}
+;
+function rm(file) {
+    fs.unlink(file, function (err) {
+        if (err)
+            throw err;
+        console.log("".concat(file, " was deleted"));
+    });
+}
+;
+switch (cmdarray[0]) {
+    case "ls":
+        ls(cmdarray[1]);
+        break;
+    case "cp":
+        cp(cmdarray[1], cmdarray[2]);
+        break;
+    case "mv":
+        mv(cmdarray[1], cmdarray[2]);
+        break;
+    case "mkdir":
+        mkdir(cmdarray[1]);
+        break;
+    case "rm":
+        rm(cmdarray[1]);
+        break;
+    case "pwd":
+        pwd();
+        break;
+    default:
+        console.log("Invalid command");
+        break;
+}
